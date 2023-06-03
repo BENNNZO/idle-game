92722edadadd070de07b332ac103daecf41c48ed
Benjamin Phillips <benjamingt537@gmail.com>
2023-06-02 21:17:57 -0400

    designed pizza slice in affinity designer as main clicker things

---
+++ src/app/layout.jsx
@@ -0,0 +1,17 @@
+import './globals.css'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({ subsets: ['latin'], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] })
+
+export const metadata = {
+  title: 'Idle Game',
+  description: 'NextJs Idle Game with Reeact Too!!',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  )
+}
